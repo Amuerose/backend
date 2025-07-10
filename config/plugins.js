2c2f3e9ef9b52b19d2c24d3d3e1458e4b6002512
Давид <david@Mac.home>
2025-07-10 14:09:08 +0200

    Initial commit of Strapi backend

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+export default {
+  // Включаем Content-Type Builder
+  'content-type-builder': {
+    enabled: true,
+  },
+  // Если используете i18n, убедитесь, что и он включён:
+  i18n: {
+    enabled: true,
+  },
+  // Другие плагины…
+};
